4ec73b29b07a1fd54c70933f0ab8d0c1a6b37109
Gustavo_Siqueira <gustavo@id360.com.br>
2023-06-11 15:11:45 -0300

    fix: adjust loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -6,17 +6,19 @@ import Image from 'next/image'
 interface LoadingProps {}
 export default function Loading(props: LoadingProps) {
   return (
-    <S.LoadingSection>
-      <G.Container>
-        <S.LoadingWrapper>
-          <Image
-            src={loadingImg}
-            priority={true}
-            alt="uma imagem animada de 4 quadrados animados desempenhando o papel de um carregamento"
-          />
-          <h2>Aguarde, estamos gerando o seu cartão de visíta!</h2>
-        </S.LoadingWrapper>
-      </G.Container>
-    </S.LoadingSection>
+    <main>
+      <S.LoadingSection>
+        <G.Container>
+          <S.LoadingWrapper>
+            <Image
+              src={loadingImg}
+              priority={true}
+              alt="uma imagem animada de 4 quadrados animados desempenhando o papel de um carregamento"
+            />
+            <h2>Aguarde, estamos gerando o seu cartão de visíta!</h2>
+          </S.LoadingWrapper>
+        </G.Container>
+      </S.LoadingSection>
+    </main>
   )
 }
